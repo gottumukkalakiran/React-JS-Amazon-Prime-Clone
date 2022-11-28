de9db62a7aea327c88416482d57b5137b4d157f2
gottumukkalakiran <gkirang4@gmail.com>
2022-11-28 15:55:44 +0000

    Prime video clone

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,39 @@
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player'
+import {IoMdClose} from 'react-icons/io'
+import 'reactjs-popup/dist/index.css'
+import './index.css'
+
+const MovieItem = props => {
+  const {movieDetails} = props
+  const {thumbnailUrl, videoUrl} = movieDetails
+
+  return (
+    <div className="movie-item-container">
+      <Popup
+        modal
+        trigger={
+          <img className="thumbnail-url" src={thumbnailUrl} alt="thumbnail" />
+        }
+        className="popup-content"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              className="close-button"
+              type="button"
+              onClick={() => close()}
+            >
+              <IoMdClose size={20} color="#231f20" />
+            </button>
+            <div className="movie-player-container">
+              <ReactPlayer url={videoUrl} controls />
+            </div>
+          </div>
+        )}
+      </Popup>
+    </div>
+  )
+}
+
+export default MovieItem
